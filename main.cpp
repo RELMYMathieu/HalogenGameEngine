bbf8cc22620f620081977c30c4882f1b85d62588
RELMY Mathieu <ninjacowzx@gmail.com>
2023-06-14 21:13:31 +0200

    fix dumb cmakelists mistakes;

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Halogen Game Engine", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Halogen Game Engine", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -16,13 +25,15 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    while (!glfwWindowShouldClose(window)) {
-        // Clear the screen, uh I forgot lol.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-        // Poll for and process events
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwPollEvents();
 
-        // Swap front and back buffers
         glfwSwapBuffers(window);
     }
 
