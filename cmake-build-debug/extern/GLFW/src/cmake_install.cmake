cead2dbb3dde0ba8209e85f6b4b900245fe4a69d
RELMY Mathieu <ninjacowzx@gmail.com>
2023-06-14 21:00:21 +0200

    create window; add GLFW

--- cmake-build-debug/extern/GLFW/src/cmake_install.cmake
+++ cmake-build-debug/extern/GLFW/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/HoaxTheSecond_/Documents/Github Repos/HalogenGameEngine
+# Install script for directory: C:/Users/HoaxTheSecond_/Documents/Github Repos/HalogenGameEngine/extern/GLFW/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,18 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Users/HoaxTheSecond_/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.9011.31/bin/mingw/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/HoaxTheSecond_/Documents/Github Repos/HalogenGameEngine/cmake-build-debug/extern/GLFW/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/HoaxTheSecond_/Documents/Github Repos/HalogenGameEngine/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Halogen Game Engine", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen, uh I forgot lol.
+
+        // Poll for and process events
+        glfwPollEvents();
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
